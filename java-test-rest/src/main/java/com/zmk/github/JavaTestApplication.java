17069748616c149d908516a5572aa689f5f3e794
张孟康 <luckyboyzmk@gmail.com>
2019-05-05 20:03:32 +0800

    update

--- java-test-rest/src/main/java/com/zmk/github/JavaTestApplication.java
+++ java-test-rest/src/main/java/com/zmk/github/JavaTestApplication.java
@@ -3,9 +3,18 @@ package com.zmk.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 public class JavaTestApplication {
+
+
+//    @RequestMapping(PATH_ROOT)
+//    public String welcome() {
+//        return "Welcome!";
+//    }
+
     public static void main(String[] args) {
         System.out.println("zmk first spring boot");
         SpringApplication.run(JavaTestApplication.class, args);
