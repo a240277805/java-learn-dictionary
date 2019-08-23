7943659f29adf8fd45112660c6286770e93d4f73
张孟康 <luckyboyzmk@gmail.com>
2019-08-23 16:52:30 +0800

    update

---
+++ java-test-test/src/main/java/com/zmk/github/test/treeTest.java
@@ -0,0 +1,16 @@
+package com.zmk.github.test;
+
+import org.junit.Test;
+
+/**
+ * @ClassName treeTest
+ * @Description TODO
+ * @Author zmk
+ * @Date 2019/8/1上午10:31
+ */
+public class treeTest {
+    @Test
+    public void test(){
+        System.out.println("zmk first Test");
+    }
+}
