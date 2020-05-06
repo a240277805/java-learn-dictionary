4b9ac467ec047404f83f77738cf988648a0a326e
张孟康 <luckyboyzmk@gmail.com>
2020-05-06 16:49:00 +0800

    update

--- java-test-rest/src/main/java/com/zmk/github/JavaTestApplication.java
+++ java-test-rest/src/main/java/com/zmk/github/JavaTestApplication.java
@@ -4,17 +4,10 @@ package com.zmk.github;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages = {"com"})
-@RestController
 @MapperScan(basePackages = {"com.zmk.github.mappers"})
 public class JavaTestApplication {
-    @RequestMapping("/")
-    public String home() {
-        return "Hello, Zbook!";
-    }
 
     public static void main(String[] args) {
         System.out.println("zmk first spring boot");
