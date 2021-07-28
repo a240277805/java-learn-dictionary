62b7faecce38ff679833ca1a79ae481e5f3ada9c
zhangmengkang <zhangmengkang@ctfo.com>
2021-07-28 11:35:57 +0800

    update

--- java-test-common/src/main/java/com/zmk/github/utils/MyStringUtils.java
+++ java-test-common/src/main/java/com/zmk/github/utils/MyStringUtils.java
@@ -1,10 +1,13 @@
 package com.zmk.github.utils;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
 import com.zmk.github.info.kafka.events.KafkaBodyAbstract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -14,6 +17,7 @@ import java.util.Objects;
  */
 public class MyStringUtils {
     static Logger logger = LoggerFactory.getLogger(MyStringUtils.class);
+
 
     /**
      * String 转 任何类型 ，转换不了 则返回空
@@ -30,13 +34,23 @@ public class MyStringUtils {
             logger.error("String转换类型 class 失败: value: {} ,type: {}", text, clazz.toString());
             return null;
         }
-
     }
 
-    public static void main(String[] args) {
-        KafkaBodyAbstract msg = JSON.parseObject("{\"id\":1,\"msgCreateTime\":1608107217769,\"traceId\":\"WgJpky43Mk\",\"type\":\"task\"}", KafkaBodyAbstract.class);
-
-        System.out.println(msg);
+    /**
+     * String 转 任何类型 ，转换不了 则返回空
+     *
+     * @param text
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> getTArrayValueElseNull(String text, Class<T> clazz) {
+        try {
+            return JSONObject.parseArray(text, clazz);
+        } catch (Exception ex) {
+            logger.error("String转换类型 class 失败: value: {} ,type: {}", text, clazz.toString());
+            return null;
+        }
     }
 
     /**
